a671a3d792d875eec47d7ecb21b597e2e9874389
DmitaN <dmitan@bk.ru>
2020-09-03 12:50:49 +0300

    first commit

---
+++ Get.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace robo_test
+{
+    public class Get
+    {
+        public void GetMethods(string path) {
+
+            string[] dllFiles = Directory.GetFiles(path, "*.dll");
+            foreach (var dllFile in dllFiles)
+            {
+                var asm = Assembly.LoadFrom(dllFile);
+                Type[] types = asm.GetTypes();
+                foreach (var item in types)
+                {
+                    Type myType = asm.GetType(item.ToString());
+                    Console.WriteLine(item.ToString());
+                    foreach (MemberInfo mi in myType.GetMembers(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+                    {
+                        Console.WriteLine($"- {mi.Name}");
+                    }
+                    foreach (MemberInfo mi in myType.GetMembers(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+                    {
+                        Console.WriteLine($"- {mi.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace robo_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var get = new Get();
+            get.GetMethods(@"C:\Users\Admin\source\repos\robo_test\bin\Debug\dll\");
+
+            Console.ReadKey();
+        }
+    }
+}
